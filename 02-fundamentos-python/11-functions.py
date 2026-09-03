41560da858dd75b2974ce78b04ee4a79b05f32be
JesusPereira-ctrl <jesuspereirap100@gmail.com>
2026-09-03 15:24:54 -0400

    feat: functions

---
+++ 02-fundamentos-python/11-functions.py
@@ -0,0 +1,10 @@
+# Parámetros
+def hello(greet='Hola', name='Invitado'):
+    print(f'{greet}, {name}')
+
+
+# Argumentos
+hello('Hola', 'Ricardo')
+hello('Ciao', 'Fernando')
+hello()
+hello(name='Teddy', greet='Hello')
