45a4083c48fec74794b2989a941b1d306351c47e
JesusPereira-ctrl <jesuspereirap100@gmail.com>
2026-09-04 00:03:55 -0400

    feat: returning a list of dictionaries in GET method

--- first-steps/src/main.py
+++ first-steps/src/main.py
@@ -2,7 +2,30 @@ from fastapi import FastAPI
 
 app = FastAPI(title='Mini Blog')
 
+BLOG_POST = [
+    {
+        'id': 1,
+        'title': 'Hola desde FastAPI',
+        'content': 'Mi primer post con FastAPI'
+    },
+    {
+        'id': 2,
+        'title': 'Mi segundo Post con FastAPI',
+        'content': 'Mi segundo post con FastAPI blablabla'
+    },
+    {
+        'id': 3,
+        'title': 'Django vs FastAPI',
+        'content': 'FastAPI es mas rápido por x razones'
+    },
+]
+
 
 @app.get('/')
 def home():
     return {'message': 'Bienvenidos a Mini Blog por Devtalles'}
+
+
+@app.get('/posts')
+def list_posts():
+    return {'data': BLOG_POST}
