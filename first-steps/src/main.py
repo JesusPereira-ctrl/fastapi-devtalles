51cec6791491bc1d8a504bfae85347d4439ed06c
JesusPereira-ctrl <jesuspereirap100@gmail.com>
2026-09-04 01:39:10 -0400

    feat: PUT method with path param and body with a example about HTTPException

--- first-steps/src/main.py
+++ first-steps/src/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Query, Body
+from fastapi import FastAPI, Query, Body, HTTPException
 
 app = FastAPI(title='Mini Blog')
 
@@ -66,3 +66,16 @@ def create_post(post: dict = Body(...)):
     }
     BLOG_POST.append(new_post)
     return {'message': 'Post creado', 'data': new_post}
+
+
+@app.put('/posts/{post_id}')
+def update_post(post_id: int, data: dict = Body(...)):
+    for post in BLOG_POST:
+        if post['id'] == post_id:
+            if 'title' in data:
+                post['title'] = data['title']
+            if 'content' in data:
+                post['content'] = data['content']
+            return {'message': 'Post actualizado', 'data': post}
+
+    raise HTTPException(status_code=404, detail='Post no encontrado')
