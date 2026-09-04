8b479776ff1bbd7f9b05bccff578fcd965c4d830
JesusPereira-ctrl <jesuspereirap100@gmail.com>
2026-09-03 23:16:13 -0400

    feat: first endpoint GET

--- first-steps/src/main.py
+++ first-steps/src/main.py
@@ -1,1 +1,8 @@
-print('Hello World')
+from fastapi import FastAPI
+
+app = FastAPI(title='Mini Blog')
+
+
+@app.get('/')
+def home():
+    return {'message': 'Bienvenidos a Mini Blog por Devtalles'}
