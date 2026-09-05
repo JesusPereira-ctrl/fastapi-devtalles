c5b71430c8de9d1be58744cc67ac36ec65e839eb
JesusPereira-ctrl <jesuspereirap100@gmail.com>
2026-09-05 15:09:35 -0400

    feat: updating the POST http method for add the rest of the models

--- first-steps/src/main.py
+++ first-steps/src/main.py
@@ -374,7 +374,36 @@ def get_post(
 
 @app.post('/posts', response_model=PostPublic, response_description='Post creado (OK)', status_code=status.HTTP_201_CREATED)
 def create_post(post: PostCreate, db: Session = Depends(get_db)):
-    new_post = PostORM(title=post.title, content=post.content)
+    author_obj = None
+    if post.author:
+        author_obj = db.execute(
+            select(AuthorORM).where(AuthorORM.email == post.author.email)
+        ).scalar_one_or_none()
+
+        if not author_obj:
+            author_obj = AuthorORM(
+                name=post.author.name,
+                email=post.author.email
+            )
+            db.add(author_obj)
+            db.flush()
+
+    new_post = PostORM(
+        title=post.title,
+        content=post.content,
+        author=author_obj
+    )
+
+    for tag in post.tags:
+        tag_obj = db.execute(
+            select(TagORM).where(TagORM.name.ilike(tag.name))
+        ).scalar_one_or_none()
+        if not tag_obj:
+            tag_obj = TagORM(name=tag.name)
+            db.add(tag_obj)
+            db.flush()
+        new_post.tags.append(tag_obj)
+
     try:
         db.add(new_post)
         db.commit()
