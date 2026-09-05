036ab75cb022fbaeb5515f2db588ef9a2d01db69
JesusPereira-ctrl <jesuspereirap100@gmail.com>
2026-09-05 13:23:41 -0400

    feat: get one post from db with SQLAlchemy

--- first-steps/src/main.py
+++ first-steps/src/main.py
@@ -246,6 +246,8 @@ class PostSummary(BaseModel):
     id: int
     title: str
 
+    model_config = ConfigDict(from_attributes=True)
+
 
 class PaginatedPost(BaseModel):
     page: int
@@ -383,15 +385,19 @@ def get_post(
     include_content: bool = Query(
         default=True,
         description='Incluir o no el contenido'
-    )
+    ),
+    db: Session = Depends(get_db)
 ):
-    for post in BLOG_POST:
-        if post['id'] == post_id:
-            if not include_content:
-                return {'id': post['id'], 'title': post['title']}
-            return post
+    post_find = select(PostORM).where(PostORM.id == post_id)
+    post = db.execute(post_find).scalar_one_or_none()
 
-    raise HTTPException(status_code=404, detail='Post no encontrado')
+    if not post:
+        raise HTTPException(status_code=404, detail='Post no encontrado')
+
+    if include_content:
+        return PostPublic.model_validate(post, from_attributes=True)
+
+    return PostSummary.model_validate(post, from_attributes=True)
 
 
 @app.post('/posts', response_model=PostPublic, response_description='Post creado (OK)', status_code=status.HTTP_201_CREATED)
