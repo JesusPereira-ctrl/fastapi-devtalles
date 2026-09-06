f78df48f2c74c0368bff3334f8827d0343dfd20e
JesusPereira-ctrl <jesuspereirap100@gmail.com>
2026-09-06 01:25:03 -0300

    feat: security in fastapi with OAuth2PasswordBearer

--- first-steps/src/api/v1/posts/router.py
+++ first-steps/src/api/v1/posts/router.py
@@ -6,6 +6,7 @@ from typing import List, Optional, Union, Literal
 from src.core.db import get_db
 from .schemas import PostPublic, PaginatedPost, PostCreate, PostUpdate, PostSummary
 from .repository import PostRepository
+from src.core.security import oauth2_scheme
 
 router = APIRouter(prefix='/posts', tags=['posts'])
 
@@ -193,3 +194,8 @@ def delete_post(post_id: int, db: Session = Depends(get_db)):
             status_code=500,
             detail='Error al eliminar el post'
         )
+
+
+@router.get('/secure')
+def secure_endpoint(token: str = Depends(oauth2_scheme)):
+    return {'message': 'Acceso con token', 'token_recibido': token}
