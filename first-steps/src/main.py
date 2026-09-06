b5948e37269370faf9bd803043b74e923b39c4c4
JesusPereira-ctrl <jesuspereirap100@gmail.com>
2026-09-05 20:44:27 -0400

    feat: create a router with search HTTP GET method

--- first-steps/src/main.py
+++ first-steps/src/main.py
@@ -17,59 +17,6 @@ def home():
     return {'message': 'Bienvenidos a Mini Blog por Devtalles'}
 
 
-@app.get('/posts', response_model=PaginatedPost)
-def list_posts(
-    text: Optional[str] = Query(
-        default=None,
-        deprecated=True,
-        description='Parámetro obsoleto, usa \'query o search\' en su lugar.'
-    ),
-    query: Optional[str] = Query(
-        default=None,
-        description='Texto para buscar por título',
-        alias='search',
-        min_length=3,
-        max_length=50,
-        pattern=r'^[\w\sáéíóúÁÉÍÓÚüÜ-]+$'
-    ),
-    per_page: int = Query(
-        default=10,
-        ge=1,
-        le=50,
-        description='Número de resultados (1-50)'
-    ),
-    page: int = Query(
-        default=1,
-        ge=1,
-        description='Número de página (>=1)'
-    ),
-    order_by: Literal['id', 'title'] = Query(
-        default='id',
-        description='Campo de orden'
-    ),
-    direction: Literal['asc', 'desc'] = Query(
-        default='asc',
-        description='Dirección de orden'
-    ),
-    db: Session = Depends(get_db)
-):
-    has_prev = current_page > 1
-    has_next = current_page < total_pages if total_pages > 0 else False
-
-    return PaginatedPost(
-        page=current_page,
-        per_page=per_page,
-        total=total,
-        total_pages=total_pages,
-        has_prev=has_prev,
-        has_next=has_next,
-        order_by=order_by,
-        direction=direction,
-        search=query,
-        items=items
-    )
-
-
 @app.get('/posts/by-tags', response_model=List[PostPublic])
 def filter_by_tags(
     tags: List[str] = Query(
