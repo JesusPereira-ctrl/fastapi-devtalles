c821c59edb6bcb4a1475a09756d41bcf4638fdf5
JesusPereira-ctrl <jesuspereirap100@gmail.com>
2026-09-07 00:24:36 -0300

    feat: sync and async endpoint commented

--- first-steps/src/api/v1/posts/router.py
+++ first-steps/src/api/v1/posts/router.py
@@ -1,6 +1,3 @@
-import asyncio
-import threading
-import time
 from math import ceil
 from typing import Annotated, Literal
 
@@ -17,18 +14,18 @@ from .schemas import PaginatedPost, PostCreate, PostPublic, PostSummary, PostUpd
 router = APIRouter(prefix="/posts", tags=["posts"])
 
 
-@router.get("/sync")
-def sync_endpoint():
-    print("SYNC thread:", threading.current_thread().name)
-    time.sleep(8)
-    return {"message": "Función sincrona termino"}
+# @router.get("/sync")
+# def sync_endpoint():
+#     print("SYNC thread:", threading.current_thread().name)
+#     time.sleep(8)
+#     return {"message": "Función sincrona termino"}
 
 
-@router.get("/async")
-async def async_endpoint():
-    print("ASYNC thread:", threading.current_thread().name)
-    await asyncio.sleep(8)
-    return {"message": "Función asíncrona termino"}
+# @router.get("/async")
+# async def async_endpoint():
+#     print("ASYNC thread:", threading.current_thread().name)
+#     await asyncio.sleep(8)
+#     return {"message": "Función asíncrona termino"}
 
 
 @router.get("", response_model=PaginatedPost)
