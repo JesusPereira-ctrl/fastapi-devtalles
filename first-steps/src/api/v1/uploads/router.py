3b2a8030c122f46996821243f3e85fe9d4249944
JesusPereira-ctrl <jesuspereirap100@gmail.com>
2026-09-07 02:28:49 -0300

    feat: uploading files in a POST Http method

--- first-steps/src/api/v1/uploads/router.py
+++ first-steps/src/api/v1/uploads/router.py
@@ -1,8 +1,13 @@
+import os
+import shutil
+import uuid
 from typing import Annotated
 
-from fastapi import APIRouter, File, UploadFile
+from fastapi import APIRouter, File, HTTPException, UploadFile, status
 
 router = APIRouter(prefix="/upload", tags=["uploads"])
+
+MEDIA_DIR = "src/media"
 
 
 @router.post("/bytes")
@@ -13,3 +18,25 @@ async def upload_bytes(file: Annotated[bytes, File(...)]):
 @router.post("/file")
 async def upload_file(file: UploadFile):
     return {"file_name": file.filename, "content_type": file.content_type}
+
+
+@router.post("/save")
+def save_file(file: UploadFile):
+    if file.content_type not in ["image/png", "image/jpeg"]:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Solo se permiten imágenes PNG o JPEG",
+        )
+
+    ext = os.path.splitext(file.filename)[1]  # .png, .jpeg
+    file_name = f"{uuid.uuid4().hex}{ext}"
+    file_path = os.path.join(MEDIA_DIR, file_name)
+
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    return {
+        "file_name": file_name,
+        "content_type": file.content_type,
+        "url": f"/media/{file_name}",
+    }
